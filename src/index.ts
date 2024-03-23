f588c3548ef6977a1333561b543183acf5bb702c
pavankumar-v <pavankumarv124@gmail.com>
2024-03-23 16:35:57 +0530

    perf(eslint): autofix formatting eslint

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,40 @@
-import dotenv from "dotenv";
-import bodyParser from "body-parser";
-import cors from "cors";
+import express, { type Express, type Request, type Response } from 'express'
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 import { checkJwt } from './middleware/auth'
 
 const env = process.env.NODE_ENV ?? 'development'
 dotenv.config({ path: `.env.${env}` })
 dotenv.config({ path: '.env' })
 
-dotenv.config();
-
-const app: Express = express();
+const app: Express = express()
 // Enable CORS
-app.use(cors());
+app.use(cors())
 
 // Enable the use of request body parsing middleware
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
-    extended: true,
+    extended: true
   })
-);
-
 )
 
 // create timesheets API endpoint - code omitted
-app.get('/protected', checkJwt, function(req, res){
-
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+app.get('/protected', checkJwt, function (req, res) {
   // Save the timesheet to the database...
 
-  //send the response
-  res.status(201).send("hello");
-});
+  // send the response
+  res.status(201).send('hello')
+})
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Running");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Running')
+})
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ?? 3000
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+  console.log(`[server]: Server is running at http://localhost:${port}`)
+})
