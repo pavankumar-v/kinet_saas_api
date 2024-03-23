14183b16c8b0c6adf2aac6d6f7051793a66ca888
pavankumar-v <pavankumarv124@gmail.com>
2024-03-23 16:34:04 +0530

    chore(middleware/auth): move jwt auth middleware to middleware/auth.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cors from "cors";
-import { expressjwt as jwt } from "express-jwt";
+import { checkJwt } from './middleware/auth'
 
-const jwksRsa = require("jwks-rsa");
 
 dotenv.config();
 
@@ -20,22 +18,7 @@ app.use(
   })
 );
 
-// Create middleware for checking the JWT
-const checkJwt = jwt({
-  // Dynamically provide a signing key based on the kid in the header and the signing keys provided by the JWKS endpoint
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://kinet-dev1024.us.auth0.com/.well-known/jwks.json`,
-  }),
-
-  // Validate the audience and the issuer
-  audience: "https://kinet-dev1024.us.auth0.com/api/v2/", //replace with your API's audience, available at Dashboard > APIs
-  issuer: "https://kinet-dev1024.us.auth0.com/",
-  algorithms: ["RS256"],
-});
-
+)
 
 // create timesheets API endpoint - code omitted
 app.get('/protected', checkJwt, function(req, res){
