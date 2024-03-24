748e0f0a7e056e8881824f9501c0c5b617e6d1bd
pavankumar-v <pavankumarv124@gmail.com>
2024-03-24 21:30:21 +0530

    chore(husky test): testing husky pre commit

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,36 @@
-import express, { type Express, type Request, type Response } from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import { checkJwt } from './middleware/auth'
-import './config'
+import express, { type Express, type Request, type Response } from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { checkJwt } from './middleware/auth';
+import './config';
 
-
-const app: Express = express()
+const app: Express = express();
 // Enable CORS
-app.use(cors())
+app.use(cors());
 
 // Enable the use of request body parsing middleware
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-)
+    bodyParser.urlencoded({
+        extended: true,
+    }),
+);
 
 // create timesheets API endpoint - code omitted
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 app.get('/protected', checkJwt, function (req, res) {
-  // Save the timesheet to the database...
+    // Save the timesheet to the database...
 
-  // send the response
-  res.status(201).send('hello')
-})
+    // send the response
+    res.status(201).send('hello');
+});
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Running')
-})
+    res.send('Running');
+});
 
-const port = process.env.PORT ?? 3000
+const port = process.env.PORT ?? 3000;
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`)
-})
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
