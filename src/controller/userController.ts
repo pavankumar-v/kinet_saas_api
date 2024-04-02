79357683dda874b7eafba9118851715a621e84bc
pavankumar-v <pavankumarv124@gmail.com>
2024-04-02 23:52:31 +0530

    refactor(srp): move create user responsibility to controller class action

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -5,10 +5,21 @@ const User = prisma.user;
 export class userController {
     static createUser = async (req: Request, res: Response) => {
         try {
-            const user = User.create({
-                data: req.body['user'],
+            const { email, given_name, family_name, auth0_id } = req.body['user'];
+            const user = await User.findUnique({
+                where: { auth0_id: auth0_id },
             });
-            res.status(200).send(user);
+            if (user) {
+                res.status(201).send('user exists');
+            } else {
+                const newUser = await User.create({
+                    data: { email, family_name, given_name, auth0_id },
+                }).catch((err) => {
+                    console.log(err);
+                });
+
+                res.status(201).json({ newUser });
+            }
         } catch (error) {
             res.status(500).json(error);
         }
